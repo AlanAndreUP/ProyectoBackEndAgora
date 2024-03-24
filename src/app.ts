6dfd92cc6042e792c98a355d060eae836fd2f7f8
Alan Andre <114703935+AlanAndreUP@users.noreply.github.com>
2024-03-24 17:54:45 -0600

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -9,9 +9,7 @@ import userRoutes from "./users/infraestructure/userRoutes";
 export const createServer = (port: number) => {
     const app: Application = express();
     app.use(express.json());
-  app.use(cors({
-        origin: '*' 
-    }));
+    app.use(cors()); 
     app.use(morgan('dev'));
     app.use(helmet());
     app.use(express.urlencoded({ extended: true }));
