30d646a380f49b5a010769b610e1bf4a0bfa7535
Alan Andre <114703935+AlanAndreUP@users.noreply.github.com>
2024-03-25 15:48:37 -0600

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import database from "./database/database";
 import { createServer } from "./app";
-
+const PORT = process.env.PORT || 3000;
 export const startApp = () => {
-    const port = Number(process.env.PORT);
+    const port = Number(PORT);
 
     database().connect();
     createServer(port).run();
